23a0298c12213cf8ef124547a8a1a8ada632a9b7
ShadonPC <55762492+Shad0n@users.noreply.github.com>
2021-10-31 20:26:20 +0300

    test_1 and __init__ method created

--- testing-pr3/guesser.py
+++ testing-pr3/guesser.py
@@ -1,1 +1,6 @@
 # программа с ядром игры
+
+class Guesser:
+    def __init__(self, word):
+        self.word = word
+        self.opened = ['?' for _ in range(len(word))]
--- testing-pr3/test.py
+++ testing-pr3/test.py
@@ -1,1 +1,17 @@
 # программа для тестирования модулей
+
+import unittest
+
+from guesser import Guesser
+
+class TestGuesser(unittest.TestCase):
+
+    def setUp(self):
+        self.guesser = Guesser("word")
+
+    def test_1(self):
+        self.assertEqual(self.guesser.is_geussed(), False)
+        pass
+
+    if __name__ == "__main__":
+        unittest.main()
