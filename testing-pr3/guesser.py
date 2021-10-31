11596501b134d9ad67fc293ce56608c16f20b4a0
ShadonPC <55762492+Shad0n@users.noreply.github.com>
2021-10-31 20:34:59 +0300

    test_2 and method for test_1 created

--- testing-pr3/guesser.py
+++ testing-pr3/guesser.py
@@ -4,3 +4,9 @@ class Guesser:
     def __init__(self, word):
         self.word = word
         self.opened = ['?' for _ in range(len(word))]
+
+    def is_geussed(self):
+        for ch in self.opened:
+            if ch == '?':
+                return False
+        return True
--- testing-pr3/test.py
+++ testing-pr3/test.py
@@ -13,5 +13,12 @@ class TestGuesser(unittest.TestCase):
         self.assertEqual(self.guesser.is_geussed(), False)
         pass
 
+    def test_2(self):
+        self.guesser.open_char('w')
+        self.assertEqual(self.guesser.get_opened(), 'w???')
+        self.guesser.open_char('o')
+        self.assertEqual(self.guesser.get_opened(), 'wo??')
+        pass
+
     if __name__ == "__main__":
         unittest.main()
